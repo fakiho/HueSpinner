5145e9530f43fb114f05666d3e1da983e71f7548
Ali Fakih <afdetails@gmail.com>
2022-05-13 12:32:21 +0200

    FEAT: Create package HueSpinner

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "HueSpinner",
+    platforms: [.iOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,8 +22,5 @@ let package = Package(
         .target(
             name: "HueSpinner",
             dependencies: []),
-        .testTarget(
-            name: "HueSpinnerTests",
-            dependencies: ["HueSpinner"]),
     ]
 )
